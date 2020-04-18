d55d080cd05b1094a123595cdfca95462ef3c156
Mr-Weik <996168543@qq.com>
2020-04-18 12:13:24 +0800

    补充

--- src/main/java/majiang/community/controller/AuthorizeController.java
+++ src/main/java/majiang/community/controller/AuthorizeController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class AuthorizeController {
@@ -17,7 +19,6 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
-
 
 
     @Value("${github.client.id}")
@@ -31,17 +32,25 @@ public class AuthorizeController {
 
 
     @GetMapping("/callback")
-    public String callbacK(@RequestParam(name = "code")String code,
-                            @RequestParam(name = "state") String state){
+    public String callbacK(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AcctssTokenDTO acctssTokenDTO = new AcctssTokenDTO();
         acctssTokenDTO.setClient_id("c80124bcc46b8f513898");
         acctssTokenDTO.setClient_secret("010a7709edcc04ede05858691d5a9e3cb7961550");
+        acctssTokenDTO.setCode(code);
         acctssTokenDTO.setCode(code);
         acctssTokenDTO.setRedirect_uri("http://localhost:8887/callback");
         acctssTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(acctssTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
-    }
+        if (user != null) {
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
+      }
 }
